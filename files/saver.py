139961e58ce1930a8eb896b919c075e4acafa2a8
KamOri1 <iknowthis45@gmail.com>
2024-08-30 21:03:34 +0200

    feat(saver): create encrypted class

---
+++ files/saver.py
@@ -0,0 +1,10 @@
+class Encryption:
+    def __init__(self, message):
+        self.message = message
+
+    def encrypt_message_rot13(self):
+        pass
+
+    def encrypt_message_rot47(self):
+        pass
+
