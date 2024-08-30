2b38b364c4e2d696c2cc51ad6acc929f5930d56e
KamOri1 <iknowthis45@gmail.com>
2024-08-30 22:36:04 +0200

    feat(Encryption): add encrypt_message_rot13 metod logic

--- files/encryption_file.py
+++ files/encryption_file.py
@@ -1,9 +1,15 @@
-class Encryption:
-    def __init__(self, message: str) -> None:
-        self.message = message
+import string
 
-    def encrypt_message_rot13(self):
-        pass
+class Encryption:
+    ALPHABET = string.ascii_lowercase
+    def __init__(self, message: str) -> None:
+        self.message = message.lower()
+
+    def encrypt_message_rot13(self)-> str:
+        shifted_alphabet = self.ALPHABET[13:] + self.ALPHABET[:13]
+        encrypted = str.maketrans(self.ALPHABET, shifted_alphabet)
+        encrypted_message = self.message.translate(encrypted)
+        return encrypted_message
 
     def encrypt_message_rot47(self):
         pass
