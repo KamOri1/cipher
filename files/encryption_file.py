4dddfac34fa754f818e1ac39ebfedbd4c650029a
KamOri1 <iknowthis45@gmail.com>
2024-08-30 23:22:02 +0200

    refactor(encryption_file) add Typing and created helpers paskage

--- files/encryption_file.py
+++ files/encryption_file.py
@@ -2,15 +2,17 @@ import string
 
 class Encryption:
     ALPHABET = string.ascii_lowercase
+
     def __init__(self, message: str) -> None:
-        self.message = message.lower()
+        self.message : str = message.lower()
 
     def encrypt_message_rot13(self)-> str:
-        shifted_alphabet = self.ALPHABET[13:] + self.ALPHABET[:13]
-        encrypted = str.maketrans(self.ALPHABET, shifted_alphabet)
-        encrypted_message = self.message.translate(encrypted)
+        shifted_alphabet : str = self.ALPHABET[13:] + self.ALPHABET[:13]
+        encrypted : dict = str.maketrans(self.ALPHABET, shifted_alphabet)
+        encrypted_message : str = self.message.translate(encrypted)
+
         return encrypted_message
 
-    def encrypt_message_rot47(self):
+    def encrypt_message_rot47(self)-> str:
         pass
 
