e6f40103e2a6d668991687a9ef17523429af3c4e
KamOri1 <iknowthis45@gmail.com>
2024-08-31 22:21:55 +0200

    chore(services): added the meu class and its logic

---
+++ menus/content_menu.py
@@ -0,0 +1,9 @@
+class menu:
+    def show_menu(self)-> None:
+        menu_list : list = ['New message', 'Open message', 'Exite']
+        for index, item in enumerate(menu_list, start=1):
+            print(f'{index}. {item}')
+
+    def get_choose(self)-> str:
+        choose : str = input('What you want to do now? Choose 1-3: ')
+        return choose
