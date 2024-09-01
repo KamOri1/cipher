bb473526bd9d405d21770c6c30e87920a498377d
KamOri1 <iknowthis45@gmail.com>
2024-09-01 23:05:33 +0200

    feat(encription_file): add new way of encripted files

--- services/encryption_file.py
+++ services/encryption_file.py
@@ -2,17 +2,22 @@ import string
 
 class Encryption:
     ALPHABET = string.ascii_lowercase
+    ALPHABET_47 = ALPHABET + string.punctuation + string.digits + string.ascii_uppercase
 
-    def __init__(self, message: str) -> None:
-        self.message : str = message.lower()
-
-    def encrypt_message_rot13(self)-> str:
+    def encrypt_message_rot13(self, new_message : str)-> str:
+        message: str = new_message.lower()
         shifted_alphabet : str = self.ALPHABET[13:] + self.ALPHABET[:13]
         encrypted : dict = str.maketrans(self.ALPHABET, shifted_alphabet)
-        encrypted_message : str = self.message.translate(encrypted)
+        encrypted_message : str = message.translate(encrypted)
 
         return encrypted_message
 
-    def encrypt_message_rot47(self)-> str:
-        pass
+    def encrypt_message_rot47(self, new_message : str)-> str:
+        message: str = new_message.lower()
+        shifted_alphabet: str = self.ALPHABET_47[47:] + self.ALPHABET_47[:47]
+        encrypted: dict = str.maketrans(self.ALPHABET_47, shifted_alphabet)
+        encrypted_message: str = message.translate(encrypted)
+
+        return encrypted_message
+
 
