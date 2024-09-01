1a983fda50e2a316d1b6f68a1c7d4064a6c40a76
KamOri1 <iknowthis45@gmail.com>
2024-09-01 22:58:35 +0200

    feat(app_manager): add new function of encryption messages

---
+++ manager/app_manager.py
@@ -0,0 +1,36 @@
+from cipher.menus.content_menu import Menu
+from cipher.files.message import Message
+from cipher.services.encryption_file import Encryption
+
+class Manager(Menu):
+    def __init__(self, open_message, exite):
+        super().__init__()
+        self.new_message = Message()
+        self.encryption = Encryption()
+        self.open_message = open_message
+        self.exite = exite
+
+        self.start_menu()
+
+    def start_menu(self)-> None:
+            while True:
+                first_choose = self.show_menu()
+                match first_choose:
+                    case '1': self.show_new_message_options()
+                    case '2': pass
+                    case '3': pass
+
+                match self.new_message.encrypted_message()["rot_type"]:
+                    case 'rot13': self.encryption.encrypt_message_rot13(self.new_message.encrypted_message()["text"])
+                    case 'rot47': self.encryption.encrypt_message_rot47(self.new_message.encrypted_message()["text"])
+
+    def show_menu(self):
+        super().show_menu()
+        return self.execute()
+
+    def execute(self):
+        return super().get_choose()
+
+    def show_new_message_options(self)-> dict:
+        self.new_message.add_new_message()
+        return self.new_message.encrypted_message()
