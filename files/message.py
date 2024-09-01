3c8adc85627f50edb16b4ee840180caff85ce872
KamOri1 <iknowthis45@gmail.com>
2024-09-01 21:21:03 +0200

    feat(message): add message class

---
+++ files/message.py
@@ -0,0 +1,14 @@
+class Message:
+    def encrypted_message(self)-> dict:
+        message : dict = {
+                        'name': self.message_name,
+                        'text': self.message_content,
+                        'rot_type': self.rot_type,
+                        'status': 'encrypted'
+                         }
+        return message
+
+    def add_new_message(self)-> None:
+        self.message_name = input('Enter message name: ')
+        self.message_content = input('Enter message content: ')
+        self.rot_type = input('Choose rot13 or rot47: ')
