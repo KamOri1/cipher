9b6e52165e5f723e414a9d9ee492b50b89a70193
KamOri1 <iknowthis45@gmail.com>
2024-09-02 23:25:54 +0200

    refactor(saver): changing the way the dir  is added

--- files/saver.py
+++ files/saver.py
@@ -1,5 +1,7 @@
 import json
+from cipher.consts import FILES_DIR
+
 class SaveFile:
     def save_message(self, all_message_information: dict) -> None:
-        with open(f'encrypted_files/{all_message_information["name"]}.json', 'w') as file:
+        with open(f'{FILES_DIR}{all_message_information["name"]}.json', 'w') as file:
             json.dump(all_message_information, file)
