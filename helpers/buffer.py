7e0ef643d208aa1c90ba2a05117fa3bdf4cb05ae
KamOri1 <iknowthis45@gmail.com>
2024-09-02 11:45:46 +0200

    style(buffer): style improvement

--- helpers/buffer.py
+++ helpers/buffer.py
@@ -2,11 +2,10 @@ import json
 
 
 class Buffer:
-
     def get_all_info(self, **kwargs) -> dict:
         complex_message = kwargs
         return complex_message
+
     def message_to_json(self, message: dict) -> json:
         message_json = json.dumps(message)
         return message_json
-
