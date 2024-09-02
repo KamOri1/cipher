be1ced460e33cccf82c8681980187b9724707a1b
KamOri1 <iknowthis45@gmail.com>
2024-09-02 15:43:50 +0200

    feat(app_manager): adding function tu decryption message

---
+++ files/reader.py
@@ -0,0 +1,10 @@
+import json
+
+
+class ReadFile:
+    def read_file(self, file_name) -> dict:
+        with open(f'encrypted_files/{file_name}.json', 'r+') as file:
+                message_data = json.load(file)
+
+        return message_data
+
