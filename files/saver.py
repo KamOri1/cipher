5213f6deb48a1347caada40d1e5d3ebe2b92a696
KamOri1 <iknowthis45@gmail.com>
2024-09-02 23:34:12 +0200

    refactor(reader/saver): changed metods to @staticmethod

--- files/saver.py
+++ files/saver.py
@@ -2,6 +2,7 @@ import json
 from cipher.consts import FILES_DIR
 
 class SaveFile:
-    def save_message(self, all_message_information: dict) -> None:
+    @staticmethod
+    def save_message(all_message_information: dict) -> None:
         with open(f'{FILES_DIR}{all_message_information["name"]}.json', 'w') as file:
             json.dump(all_message_information, file)
