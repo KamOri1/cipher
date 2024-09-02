afd6510e8c89ae942fc4574974f54501bbd65d03
KamOri1 <iknowthis45@gmail.com>
2024-09-02 23:49:48 +0200

    refactor(main): changed the way of loaf encrypted/decrypted class

--- services/__init__.py
+++ services/__init__.py
@@ -1,4 +1,5 @@
-from .encryption_file import Rot13
+from .rot_13 import Rot13
+from .rot_47 import Rot47
 
 ROT_TYPE_13 = 'rot_13'
 ROT_TYPE_47 = 'rot_47'
@@ -8,5 +9,4 @@ def rot_factory(rot_type):
     if rot_type == ROT_TYPE_13:
         return Rot13()
     elif rot_type == ROT_TYPE_47:
-        ...
-        # return Rot47()
+        return Rot47()
