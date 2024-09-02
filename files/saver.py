3873e12d5d061fceb91f8521c0fc98de4379d18f
KamOri1 <iknowthis45@gmail.com>
2024-09-02 14:22:38 +0200

    style(saver): delaete unnecessary comment

--- files/saver.py
+++ files/saver.py
@@ -2,5 +2,4 @@ import json
 class SaveFile:
     def save_message(self, all_message_information):
         with open(f'files/{all_message_information["name"]}.json', 'w') as file:
-            #file.write(f'{all_message_information}\n')
             json.dump(all_message_information, file)
