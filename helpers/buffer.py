be7a5888303da7ff180dff709b7a7684c0d18106
KamOri1 <iknowthis45@gmail.com>
2024-09-03 22:19:30 +0200

    refactor(buffer): cjanged the style of saving message dict

--- helpers/buffer.py
+++ helpers/buffer.py
@@ -10,7 +10,7 @@ class Buffer:
         return complex_message
 
     def message_to_json(self, message: dict):
-        message_json = self.buffer_list.append(message)
+        message_json = self.buffer_list.append(message.__dict__)
 
         return message_json
 
