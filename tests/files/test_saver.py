2f5415875f647a12fc9db268005f90d74dc9ef46
KamOri1 <iknowthis45@gmail.com>
2024-09-25 13:04:47 +0200

    tests: add tests for message_status function

--- tests/files/test_saver.py
+++ tests/files/test_saver.py
@@ -1,8 +1,15 @@
 from cipher.files.saver import SaveFile
 
 
-def test_function_should_return_eEcrypting_str():
+def test_function_message_status_should_return_ecrypting_str():
     value = "Decrypting"
     exp = "Encrypting"
+
+    assert SaveFile.message_status(value) == exp
+
+
+def test_function_message_status_should_return_decrypting_str():
+    value = "Encrypting"
+    exp = "Decrypting"
 
     assert SaveFile.message_status(value) == exp
