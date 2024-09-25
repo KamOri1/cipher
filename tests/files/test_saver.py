46535a6ae67e2a5ca294656546d4c68518071f82
KamOri1 <iknowthis45@gmail.com>
2024-09-25 13:29:24 +0200

    tests(saver): create unitests for message_status function

--- tests/files/test_saver.py
+++ tests/files/test_saver.py
@@ -1,7 +1,7 @@
 from cipher.files.saver import SaveFile
 
 
-def test_function_message_status_should_return_ecrypting_str():
+def test_function_message_status_should_return_encrypting_str():
     value = "Decrypting"
     exp = "Encrypting"
 
@@ -11,5 +11,12 @@ def test_function_message_status_should_return_ecrypting_str():
 def test_function_message_status_should_return_decrypting_str():
     value = "Encrypting"
     exp = "Decrypting"
+
+    assert SaveFile.message_status(value) == exp
+
+
+def test_function_message_status_should_return_none():
+    value = "nothing"
+    exp = None
 
     assert SaveFile.message_status(value) == exp
