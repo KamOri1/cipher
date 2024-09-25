ab6819a78e3757931ab90c935d8a7a5572fd9c29
KamOri1 <iknowthis45@gmail.com>
2024-09-25 13:01:38 +0200

    tests: first test

--- tests/files/test_saver.py
+++ tests/files/test_saver.py
@@ -5,4 +5,4 @@ def test_function_should_return_eEcrypting_str():
     value = "Decrypting"
     exp = "Encrypting"
 
-    assert SaveFile.message_status(value) == exp  # nosec B101
+    assert SaveFile.message_status(value) == exp
