250f1d17e37bb0b4fb4f1e3bd6a11e962a55b2e0
KamOri1 <iknowthis45@gmail.com>
2024-09-28 23:25:27 +0200

    tests(content_menu): add get_choose test

--- tests/menus/tests_content_menu.py
+++ tests/menus/tests_content_menu.py
@@ -11,8 +11,21 @@ import pytest
         )
     ],
 )
-def test_content_menu_should_print_list_elements(menu_options, expected_output, capsys):
+def test_show_menu_should_print_list_elements(menu_options, expected_output, capsys):
     Menu.show_menu(menu_options)
     captured_output = capsys.readouterr().out
 
     assert captured_output == expected_output
+
+
+@pytest.mark.parametrize(
+    "user_choose, expected_output",
+    [(1, 1)],
+)
+def test_get_choose_should_return_expected_value(user_choose, expected_output, mocker):
+    mocker.patch("cipher.menus.content_menu.Menu.get_choose", return_value=user_choose)
+    Menu.choose = user_choose
+
+    result = Menu.get_choose()
+
+    assert result == expected_output
